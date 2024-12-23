ef3976735dc582bd928b898912a0112cd6c1c9e2
kirjav <89679609+kirjav@users.noreply.github.com>
2024-12-23 16:43:53 -0600

    form and new book submit as well as

--- script.js
+++ script.js
@@ -6,15 +6,38 @@ function Book(author, title, numberOfPages, readingStatus) {
     this.numberOfPages = numberOfPages;
     this.readingStatus = readingStatus;
     this.info = function() {
-        answer = this.title + "by" + this.author + "," + this.numberOfPages + ","
+        answer = this.title + " by " + this.author + ", " + this.numberOfPages + " pages, "
         answer += this.readingStatus ? " read" : " not read yet";
         return answer;
     }
 
 }
 
-function addBookToLibrary() {
-    const newBook = new Book()
-    myLibrary.append(newBook);
+function addBookToLibrary(event) {
+    event.preventDefault();
+    const form = event.target;
+    const newBook = new Book(form.book_author.value, form.book_title.value, form.book_numberOfPages.value, form.book_readingStatus.value)
+    myLibrary.push(newBook);
+    createBookCard(newBook);
+    console.log(newBook.info());
+}
+
+function createBookCard(newBook){
 
 }
+
+function openAddBookPrompt(){
+    const div = document.getElementById("formContainer");
+    div.style.display = "block";
+}
+
+function closeAddBookPrompt(){
+    const div = document.getElementById("formContainer");
+    const form = document.getElementById("newBookForm");
+    form.reset();
+    div.style.display = "none";
+
+}
+
+// Basically I will create a preset container that I can read and copy and append. 
+// //It's display will be none but when i add a gbook ill fix that
